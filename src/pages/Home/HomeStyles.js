fc6560c175011768f6aacb403976ef2446b72a23
Martin Fernandez <dosunounodev@gmail.com>
2021-05-13 19:06:27 -0300

    fix en jsonconfig y merge

---
+++ src/pages/Home/HomeStyles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 200px;
+  background-color: #764abc;
+  p {
+    font-size: 1.8rem;
+    font-weight: 600;
+    color: #fff;
+  }
+`;
+
+export { Header };
